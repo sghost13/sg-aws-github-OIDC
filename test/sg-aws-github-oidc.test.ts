653ed69ba4c3fd517d0f9a13d539b143c651ca59
sg <sg@sg-mac.local>
2024-06-19 12:32:16 -0500

    Initial commit

---
+++ test/sg-aws-github-oidc.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SgAwsGithubOidc from '../lib/sg-aws-github-oidc-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sg-aws-github-oidc-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SgAwsGithubOidc.SgAwsGithubOidcStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
