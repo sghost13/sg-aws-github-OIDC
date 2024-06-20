1d116ae87a49c192746235cc3704c1dca5d4542e
sg <sg@sg-mac.local>
2024-06-19 23:38:04 -0500

    fix

--- lib/github-deploy-role.ts
+++ lib/github-deploy-role.ts
@@ -5,82 +5,27 @@ import { Construct } from 'constructs';
 export class GithubActionsRoleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
     // Create the OIDC provider for GitHub
     const oidcProvider = new cdk.aws_iam.OpenIdConnectProvider(this, 'GithubOIDCProvider', {
       url: 'https://token.actions.githubusercontent.com',
-      clientIds: ['https://sts.amazonaws.com']
+      clientIds: ['sts.amazonaws.com']
     });
+
     // Create the role
     const role = new Role(this, 'GithubActionsRole', {
       assumedBy: new FederatedPrincipal(oidcProvider.openIdConnectProviderArn, {
         StringLike: {
-          'token.actions.githubusercontent.com:aud': 'https://sts.amazonaws.com',
+          'token.actions.githubusercontent.com:aud': 'sts.amazonaws.com',
           'token.actions.githubusercontent.com:sub': 'repo:sghost13/*:*'
         },
-        // StringLike: {
-        //   'token.actions.githubusercontent.com:sub': [
-        //     'repo:sghost13/*:ref:refs/heads/main'
-        //   ]
-        // }
       },
       'sts:AssumeRoleWithWebIdentity'
     ),
       description: 'Role for Github Actions to deploy using CDK', // Description for the role
       roleName: 'GitHubActions', // Custom name for the role
-      // maxSessionDuration: cdk.Duration.hours(1), // Maximum duration for the role session
-
-      // Inline policies attached to the role
-      // inlinePolicies: {
-      //   GithubActionsPolicy: new PolicyDocument({
-      //     statements: [
-      //       new PolicyStatement({
-      //         actions: [
-      //           'cloudformation:CreateStack',
-      //           'cloudformation:DeleteStack',
-      //           'cloudformation:DescribeStacks',
-      //           'cloudformation:UpdateStack',
-      //           'cloudformation:DescribeStackResources',
-      //           'cloudformation:DescribeStackEvents',
-      //           'cloudformation:GetTemplate',
-      //           'cloudformation:ValidateTemplate',
-      //           's3:ListBucket',
-      //           's3:GetObject',
-      //           's3:PutObject',
-      //           'iam:PassRole',
-      //           'ssm:GetParameter',
-      //           'ssm:GetParameters',
-      //           'ssm:DescribeParameters'
-      //         ],
-      //         resources: ['*'],
-      //         effect: Effect.ALLOW,
-      //         sid: 'AllowCDKDeployments'
-      //       }),
-      //       new PolicyStatement({
-      //         actions: [
-      //           'ec2:Describe*',
-      //           'ec2:CreateTags',
-      //           'ec2:DeleteTags'
-      //         ],
-      //         resources: ['*'],
-      //         effect: Effect.ALLOW,
-      //         sid: 'AllowEC2Operations'
-      //       }),
-      //       new PolicyStatement({
-      //         actions: [
-      //           'lambda:CreateFunction',
-      //           'lambda:DeleteFunction',
-      //           'lambda:InvokeFunction',
-      //           'lambda:UpdateFunctionCode',
-      //           'lambda:UpdateFunctionConfiguration'
-      //         ],
-      //         resources: ['*'],
-      //         effect: Effect.ALLOW,
-      //         sid: 'AllowLambdaOperations'
-      //       })
-      //     ]
-      //   })
-      // }
     });
+
     // Additional managed policies (optional)
 //    role.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess'));
   }
