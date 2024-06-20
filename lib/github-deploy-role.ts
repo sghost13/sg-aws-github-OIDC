46bb6db54f8b96e44ac5d1d223fe6fbbd3aaa005
sg <sg@sg-mac.local>
2024-06-20 01:11:28 -0500

    fix

--- lib/github-deploy-role.ts
+++ lib/github-deploy-role.ts
@@ -15,22 +15,75 @@ export class GithubActionsRoleStack extends cdk.Stack {
     // Create the role
     const role = new Role(this, 'GithubActionsRole', {
       assumedBy: new FederatedPrincipal(oidcProvider.openIdConnectProviderArn, {
-        StringLike: {
+        StringEquals: {
           'token.actions.githubusercontent.com:aud': 'sts.amazonaws.com',
           'token.actions.githubusercontent.com:sub': [
-//            'repo:<USER>/<REPO>:*<BRANCH>'
-            'repo:sghost13/sg-aws-github-oidc:*main'
+              'repo:sghost13/sg-aws-github-oidc:environment:dev'
           ]
         },
+        /** explicit branch naming like docs say does not work
+        *   ie: repo:<USER>/<REPO>:ref/refs/heads/<BRANCH> will not work
+        **/
+//         StringLike:{
+//           'token.actions.githubusercontent.com:sub': [
+// //            'repo:<USER>/<REPO>:*'
+//             'repo:sghost13/sg-aws-github-oidc:*'
+//           ]
+//         },
       },
       'sts:AssumeRoleWithWebIdentity'
     ),
       description: 'Role for Github Actions to deploy using CDK', // Description for the role
       roleName: 'GitHubActions', // Custom name for the role
+      maxSessionDuration: cdk.Duration.hours(1), // Maximum duration for the role session
+      // Inline policies attached to the role
+      inlinePolicies: {
+        GithubActionsPolicy: new PolicyDocument({
+          statements: [
+            new PolicyStatement({
+              actions: [
+                'cloudformation:*',
+                's3:ListBucket',
+                's3:GetObject',
+                's3:PutObject',
+                'iam:PassRole',
+                'ssm:GetParameter',
+                'ssm:GetParameters',
+                'ssm:DescribeParameters'
+              ],
+              resources: ['*'],
+              effect: Effect.ALLOW,
+              sid: 'AllowCDKDeployments'
+            }),
+            new PolicyStatement({
+              actions: [
+                'ec2:Describe*',
+                'ec2:CreateTags',
+                'ec2:DeleteTags'
+              ],
+              resources: ['*'],
+              effect: Effect.ALLOW,
+              sid: 'AllowEC2Operations'
+            }),
+            new PolicyStatement({
+              actions: [
+                'lambda:CreateFunction',
+                'lambda:DeleteFunction',
+                'lambda:InvokeFunction',
+                'lambda:UpdateFunctionCode',
+                'lambda:UpdateFunctionConfiguration'
+              ],
+              resources: ['*'],
+              effect: Effect.ALLOW,
+              sid: 'AllowLambdaOperations'
+            })
+          ]
+        })
+      }
     });
 
     // Additional managed policies (optional)
-    role.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess'));
+//    role.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess'));
   }
 }
 
